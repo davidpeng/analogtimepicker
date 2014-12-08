f5988edae1507245186e8bccffebac65157c22cd
David Peng <dp@z10.us>
2014-12-07 20:49:14 -0800

    Major refactor to remove dependency on jQuery.

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ var uglify = require('gulp-uglify');
 
 gulp.task('css', function() {
   return gulp.src('src/*.css')
-    .pipe(concat('jquery.analogtimepicker.css'))
+    .pipe(concat('analogtimepicker.css'))
     .pipe(gulp.dest('dist'))
     .pipe(minifyCss())
-    .pipe(rename('jquery.analogtimepicker.min.css'))
+    .pipe(rename('analogtimepicker.min.css'))
     .pipe(gulp.dest('dist'));
 });
 
@@ -18,10 +18,10 @@ gulp.task('js', function() {
   return gulp.src('src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(concat('jquery.analogtimepicker.js'))
+    .pipe(concat('analogtimepicker.js'))
     .pipe(gulp.dest('dist'))
     .pipe(uglify())
-    .pipe(rename('jquery.analogtimepicker.min.js'))
+    .pipe(rename('analogtimepicker.min.js'))
     .pipe(gulp.dest('dist'));
 });
 
